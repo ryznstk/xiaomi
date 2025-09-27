544c3472b96189caf50c4d79cddc84aac06e17c7
zenin1504 <kayozzprojects@yandex.com>
2025-09-27 13:29:56 +0000

    peridot: Add KProfiles support
    Change-Id: Ib066a8582a1f40598eb2df627ef1c953
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -285,6 +285,10 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_peridot)
 
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint3-service.strongbox.nxp \
