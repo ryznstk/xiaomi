9f4401d0d82abaff1109cb841a04e6cee65904be
Guidix <guidixm@gmail.com>
2025-09-30 03:03:48 +0300

    peridot: sepolicy: Label settingslib prop
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -239,6 +239,8 @@ include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include $(DEVICE_PATH)/sepolicy/SEPolicy-diag.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
