c2d980d5abbdb0c59611faff1fec1da0cd9bf42d
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-02-23 21:42:49 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,6 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/display-drivers/msm \
     qcom/opensource/dsp-kernel \
     qcom/opensource/eva-kernel \
-    qcom/opensource/fingerprint \
     qcom/opensource/video-driver \
     qcom/opensource/graphics-kernel \
     qcom/opensource/wlan/platform \
