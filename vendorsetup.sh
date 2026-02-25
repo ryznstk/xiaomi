cdcf334b18ada36c3f482245b190f130248ea78a
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-02-25 20:04:48 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@
 # Vendor (fresh clone)
 echo "Cloning vendor tree..."
 rm -rf vendor/xiaomi/peridot
-git clone -b back https://gitlab.com/blu96/vendor-xiaomi-peridot-qpr-2.git vendor/xiaomi/peridot
+git clone -b lineage-23.2 https://gitlab.com/blu96/vendor-xiaomi-peridot-rb.git vendor/xiaomi/peridot
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
