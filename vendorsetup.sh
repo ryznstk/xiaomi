66a0f02654e1a77e992ca8f7e92b40365ac9f9e5
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-03-22 09:21:33 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -69,70 +69,70 @@ git fetch https://github.com/ryznstk/build_soong test
 git reset --hard FETCH_HEAD
 croot
 
-cd external/arm-optimized-routines
-git fetch https://github.com/Lunaris-AOSP/external_arm-optimized-routines test
-git reset --hard FETCH_HEAD
-croot
+#cd external/arm-optimized-routines
+#git fetch https://github.com/Lunaris-AOSP/external_arm-optimized-routines test
+#git reset --hard FETCH_HEAD
+#croot
 
-cd device/lineage/sepolicy
-git fetch https://github.com/Lunaris-AOSP/device_lineage_sepolicy 16.2
-git reset --hard FETCH_HEAD
-croot
+#cd device/lineage/sepolicy
+#git fetch https://github.com/Lunaris-AOSP/device_lineage_sepolicy 16.2
+#git reset --hard FETCH_HEAD
+#croot
 
-cd hardware/interfaces
-git fetch https://github.com/Lunaris-AOSP/hardware_interfaces.git test
-git reset --hard FETCH_HEAD
-croot
+#cd hardware/interfaces
+#git fetch https://github.com/Lunaris-AOSP/hardware_interfaces.git test
+#git reset --hard FETCH_HEAD
+#croot
 
-cd packages/apps/Launcher3
-git fetch https://github.com/Lunaris-AOSP/packages_apps_Launcher3 test
-git reset --hard FETCH_HEAD
-croot
+#cd packages/apps/Launcher3
+#git fetch https://github.com/Lunaris-AOSP/packages_apps_Launcher3 test
+#git reset --hard FETCH_HEAD
+#croot
 
 #cd packages/apps/FaceUnlock
 #git fetch https://github.com/Lunaris-AOSP/packages_apps_FaceUnlock.git test
 #git reset --hard FETCH_HEAD
 #croot
 
-cd packages/modules/Bluetooth
-git fetch https://github.com/Lunaris-AOSP/packages_modules_Bluetooth 16.2
-git reset --hard FETCH_HEAD
-croot
+#cd packages/modules/Bluetooth
+#git fetch https://github.com/Lunaris-AOSP/packages_modules_Bluetooth 16.2
+#git reset --hard FETCH_HEAD
+#croot
 
-cd frameworks/base
-git fetch https://github.com/Lunaris-AOSP/frameworks_base test
-git reset --hard FETCH_HEAD
-croot
+#cd frameworks/base
+#git fetch https://github.com/Lunaris-AOSP/frameworks_base test
+#git reset --hard FETCH_HEAD
+#croot
 
-cd frameworks/native
-git fetch https://github.com/Lunaris-AOSP/frameworks_native test
-git reset --hard FETCH_HEAD
-croot
+#cd frameworks/native
+#git fetch https://github.com/Lunaris-AOSP/frameworks_native test
+#git reset --hard FETCH_HEAD
+#croot
 
 cd packages/apps/Settings
 git fetch https://github.com/ryznstk/packages_apps_Settings test
 git reset --hard FETCH_HEAD
 croot
 
-cd packages/apps/Singularity 
-git fetch https://github.com/Lunaris-AOSP/packages_apps_Singularity test
-git reset --hard FETCH_HEAD
-croot
+#cd packages/apps/Singularity 
+#git fetch https://github.com/Lunaris-AOSP/packages_apps_Singularity test
+#git reset --hard FETCH_HEAD
+#croot
 
-cd vendor/lineage
-git fetch https://github.com/Lunaris-AOSP/vendor_lineage 16.2
-git reset --hard FETCH_HEAD
-croot
+#cd vendor/lineage
+#git fetch https://github.com/Lunaris-AOSP/vendor_lineage 16.2
+#git reset --hard FETCH_HEAD
+#croot
 
-cd vendor/extras
-git fetch https://github.com/Lunaris-AOSP/vendor_extras test
-git reset --hard FETCH_HEAD
-croot
+#cd vendor/extras
+#git fetch https://github.com/Lunaris-AOSP/vendor_extras test
+#git reset --hard FETCH_HEAD
+#croot
 
-cd toolchain/pgo-profiles
-git fetch https://github.com/Lunaris-AOSP/toolchain_pgo-profiles 16.2
-git reset --hard FETCH_HEAD
-croot
+#cd toolchain/pgo-profiles
+#git fetch https://github.com/Lunaris-AOSP/toolchain_pgo-profiles 16.2
+#git reset --hard FETCH_HEAD
+#croot
 
 cd device/qcom/sepolicy_vndr/sm8650
 git fetch https://github.com/LineageOS/android_device_qcom_sepolicy_vndr.git lineage-23.2-caf-sm8650
