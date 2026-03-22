0cee3609e96c90d57a48a004605bad77adf9366e
ryznstk <agusmhrd1k4@gmail.com>
2026-03-22 13:12:26 +0000

    Revert "peridot: Add KProfiles support"
    This reverts commit 544c3472b96189caf50c4d79cddc84aac06e17c7.

--- device.mk
+++ device.mk
@@ -289,10 +289,6 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_peridot)
 
-# KProfiles
-PRODUCT_PACKAGES += \
-    KProfiles
-
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint3-service.strongbox.nxp \
