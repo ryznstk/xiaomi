b610d2ca3782c99f82e7e4f433df21cdd58966a6
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-03-25 06:29:04 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -115,7 +115,7 @@ git reset --hard FETCH_HEAD
 croot
 
 cd packages/apps/Singularity 
-git fetch https://github.com/Lunaris-AOSP/packages_apps_Singularity test
+git fetch https://github.com/ryznstk/packages_apps_Singularity 16.2
 git reset --hard FETCH_HEAD
 croot
 
