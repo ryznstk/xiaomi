e947321f7324c15436d1a91d64cd75a6d23e8721
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-03-30 10:49:28 +0700

    Update git fetch branch for Launcher3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -85,7 +85,7 @@ croot
 #croot
 
 cd packages/apps/Launcher3
-git fetch https://github.com/Lunaris-AOSP/packages_apps_Launcher3 test
+git fetch https://github.com/Lunaris-AOSP/packages_apps_Launcher3 16.2
 git reset --hard FETCH_HEAD
 croot
 
