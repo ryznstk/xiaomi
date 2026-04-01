faa093e37b4ebc21eedc675d2db30f90e69b6c66
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-04-02 04:00:26 +0700

    Remove extra.config from TARGET_KERNEL_CONFIG
    Removed 'vendor/extra.config' from the kernel configuration.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,7 +124,6 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8635
 KERNEL_LTO := thin
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
-    vendor/extra.config \
     vendor/pineapple_GKI.config \
     vendor/peridot_GKI.config
 
